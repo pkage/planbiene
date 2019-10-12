d820ecdf36ea9567e3dbc140850ac7c53fc3fbaf
Liana <lianafaahmed@gmail.com>
2019-10-12 23:15:03 +0200

    Added currency converter

---
+++ currency.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+baseURI = 'https://api.exchangeratesapi.io/latest?base=%s&symbols=EUR'
+
+def getRate(currency):
+
+    response = json.loads(requests.get(baseURI%currency).content)
+    return response['rates'][currency]
+
+if __name__ == "__main__":
+    print(getRate('GBP'))
